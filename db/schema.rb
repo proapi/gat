b3409b08d8dc2225da08d3cf0d03d812aa223585
Michał Pawelski <kontakt@proapi.eu>
2015-09-27 10:29:47 +0200

    Create Location model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150927082808) do
+ActiveRecord::Schema.define(version: 20150927082915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "external_id"
+    t.string   "secret_code"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "panel_providers", force: :cascade do |t|
     t.string   "code"
